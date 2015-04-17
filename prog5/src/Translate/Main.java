6bf69ec52e7699824412539811a0207011959a6e
Jack Weinnig <jackweinnig@gmail.com>
2015-04-17 00:04:35 -0500

    Init commit

--- prog5/src/Translate/Main.java
+++ prog5/src/Translate/Main.java
@@ -8,7 +8,7 @@ public class Main {
   public static void main(String argv[])  {
     for (int i = 0; i < argv.length; ++i) {
       //String filename = argv[i];
-    	String filename = "/Users/jack/Dev/Eclipse/prog5/src/test.tig";
+    	String filename = "/Users/jack/git/CSC4351P5/prog5/src/test.tig";
       if (argv.length > 1)
 	System.out.println("***Processing: " + filename);
       Parse parse = new Parse(filename);
